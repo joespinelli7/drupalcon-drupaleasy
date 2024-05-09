8c23eb1125d04af94b2424509bc3530dc82432cb
Joe Spinelli <joe.spinelli@hidglobal.com>
2024-05-09 13:12:43 -0500

    custom: Add commented notes to hello world module

--- web/modules/custom/hello_world/src/Controller/HelloWorldController.php
+++ web/modules/custom/hello_world/src/Controller/HelloWorldController.php
@@ -1,5 +1,6 @@
 <?php
 
+// PHP declaration to enforce strict types
 declare(strict_types=1);
 
 namespace Drupal\hello_world\Controller;
@@ -8,11 +9,14 @@ use Drupal\Core\Controller\ControllerBase;
 
 /**
  * Returns responses for Hello world! routes.
+ * final means no one can extend this class
  */
 final class HelloWorldController extends ControllerBase {
 
   /**
    * Builds the response.
+   * __invoke() means this function will immediately be run if this class is ever called on.
+   * that way you don't have to specifically call any function inside the .routing.yml file
    */
   public function __invoke(): array {
 
